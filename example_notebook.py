1860eadc7810309d8843eb45c7d5d8d76dc1e2c8
Doug Anson <doug.anson@arm.com>
2021-06-30 16:43:50 -0500

    more fixes to json tensor processing

--- example_notebook.py
+++ example_notebook.py
@@ -29,18 +29,6 @@ from pelion_sagemaker_controller import pelion_sagemaker_controller
 
 class MyNotebook:
     def __init__(self, api_key, device_id, endpoint_api, aws_s3_folder):
-        # Some misc settings we'll use in the analysis portions
-        self.num_bytes = 5
-
-        # Float formatting...
-        self.float_format_str = "{:.8f}"
-        
-        # Float unpack format
-        self.endian_format_str = '<f'
-        
-        # Float byte length 
-        self.float_bytelen = 4
-        
         # Initialize Sagemaker
         self.sagemaker_init(aws_s3_folder)
         
@@ -107,8 +95,9 @@ class MyNotebook:
         return model_basename + '.tar.gz'
         
     # Compile model and package/upload to S3
-    def compile_model(self, created_model, target_device, model_basename, data_shape, framework):
+    def compile_model(self, created_model, target_device, model_basename, framework, data_shape):
         # Announce long winded task
+        print("")
         print("Beginning model compilation...")
         
         # Record the allocated model
@@ -121,8 +110,10 @@ class MyNotebook:
         keras_model_path = self.sess.upload_data(packaged_model_filename, self.bucket, self.folder)
 
         keras_compilation_job_name = 'Sagemaker-Edge-'+ str(time.time()).split('.')[0]
-        print('Compilation job for %s started' % keras_compilation_job_name)
-
+        
+        # Initiate the compilation job
+        print("")
+        print('Compilation job (%s) has started...' % keras_compilation_job_name)
         response = sagemaker_client.create_compilation_job(
                 CompilationJobName=keras_compilation_job_name,
                 RoleArn=self.role,
@@ -155,7 +146,7 @@ class MyNotebook:
         print('Done!')
         return keras_compilation_job_name
 
-    # Package model:  keras-model-1.0.tar.gz
+    # package up the model as tgz for transport via Pelion to Sagemaker Edge Agent service
     def package_model(self, keras_packaged_model_name, keras_model_version, keras_compilation_job_name):
         # Announce long winded task
         print("Beginning model packaging...")
@@ -198,65 +189,16 @@ class MyNotebook:
         with open(local_output_tensor_filename, 'wb') as f:
             self.s3_client.download_fileobj(self.bucket, output_tensor_filename, f)
     
-    # JSON Tensor to Numpy Tensor (float)        
-    def json_tensor_to_numpy_float_tensor(self, json_tensor):
-        # Convert our JSON-based output tensor to a numpy tensor (with byte values initially...)
-        tensor_byte_data_np = np.frombuffer(base64.b64decode(json_tensor['b64_data']), dtype=np.dtype('B'))
-
-        # Reshape our numpy tensor per the number of imput images 
-        rs_tensor_byte_data_np = np.reshape(tensor_byte_data_np,[json_tensor['shape'][0],(json_tensor['shape'][1]*self.float_bytelen)])
-        print("JsonTensorToNumpyFloatTensor: Reshaped: " + str(tensor_byte_data_np.shape) + " -> " + str(rs_tensor_byte_data_np.shape))
-
-        # Convert to Floats each of 4 bytes, then to numpy float array
-        float_array = []
-        length = rs_tensor_byte_data_np.shape[0]
-        ie_length = rs_tensor_byte_data_np.shape[1]
-        i = 0
-        while i < length:
-            image_entry = [];
-
-            j = 0
-            while j < ie_length:
-                image_entry.append(
-                    float(self.float_format_str.format(
-                        float('.'.join(str(elem) for elem in struct.unpack(self.endian_format_str, memoryview(rs_tensor_byte_data_np[i][j:(j+self.float_bytelen)])))
-                            )    
-                        )
-                    )
-                )
-                j += self.float_bytelen
-            float_array.append(image_entry)
-            i += 1
-            
-        # Return a numpy float array
-        return np.asarray(float_array) 
-
-    # Imagenet-based results display
+    # Simple Image List Display with optional annotations...
     def display_images(self, my_list, most_likely_labels=None):
         for i, img in enumerate(my_list):
             display(img)
             if most_likely_labels != None:
                 print(most_likely_labels[i])
 
-    # Read in and prepare our images for prediction processing by ResNet50 model
+    # Read in and prepare our images for prediction processing
     def read_and_prep_images(self, img_paths, img_height, img_width):
-        global image_list
-        image_list = [load_img(img_path, target_size=(img_height, img_width)) for img_path in img_paths if os.path.isfile(img_path)]
-        return np.array([img_to_array(img) for img in image_list])
-
-    # Decode our imagenet based predictions
-    def decode_predictions(self, preds, top=5, class_list_path=None):
-        if len(preds.shape) != 2 or preds.shape[1] != 1000:
-            raise ValueError('`decode_predictions` expects '
-                         'a batch of predictions '
-                         '(i.e. a 2D array of shape (samples, 1000)). '
-                         'Found array with shape: ' + str(preds.shape))
-        CLASS_INDEX = json.load(open(class_list_path))
-        results = []
-        for pred in preds:
-            top_indices = pred.argsort()[-top:][::-1]
-            result = [tuple(CLASS_INDEX[str(i)]) + (pred[i],) for i in top_indices]
-            result.sort(key=lambda x: x[2], reverse=True)
-            results.append(result)
-        return results
+        img_list = [load_img(img_path, target_size=(img_height, img_width)) for img_path in img_paths if os.path.isfile(img_path)]
+        array_list =  np.array([img_to_array(img) for img in img_list])
+        return {"img":img_list, "array":array_list}
     
